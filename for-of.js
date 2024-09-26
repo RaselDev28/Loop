9d70198fe98606a73885998ce72dda9dd552f7a4
Rasel <you@example.com>
2024-09-26 23:10:09 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,5 @@
+const books=['English', 'Bangla', 'Physics', 'Math', 'Science'];
+for(const book of books){
+    console.log(book);
+    console.log("I want this book!");
+}
---
+++ while.js
@@ -0,0 +1,27 @@
+// Task 1............
+// let msg=1;
+// while(msg<=60){
+//     console.log(msg,"I will invest at least 6 hrs every single day for next 60 days!");
+//     msg++;
+// }
+// Task 2..............
+// let num=1;
+// sum=0;
+// while(num<=20){
+//     console.log(num);
+//     sum=sum+num;
+//     console.log("The sum is ",sum);
+//     num++;
+//  }
+// Task 3...............
+let num=1;
+while (num<=10){
+    console.log(num);
+    if(num%2==0){
+        console.log("This is even number");
+    }
+    else{
+        console.log("This is odd number");
+    }
+    num++;
+}
